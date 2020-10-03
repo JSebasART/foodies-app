433047919ff3fa075a4cad7a9eab44767004faa3
Sebastian Jimenez <Sebasjartiga@gmail.com>
2020-10-03 10:14:21 -0600

    Enviroment ready

---
+++ src/views/Landing.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="">
+    <div id="background" class="absolute z-0 w-full">
+         <div class="h-screen">
+           <img src="@/assets/landing/vector.svg" class="float-right top-0 w-11/12 md:w-7/12 lg:w-6.5/12">
+         </div>
+    </div>
+    <div id="content" class="absolute z-10 w-full h-full">
+      <div class="bg-black mx-auto h-screen w-1 z-10"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {};
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      width:{
+        '6.5/12':'54%'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
